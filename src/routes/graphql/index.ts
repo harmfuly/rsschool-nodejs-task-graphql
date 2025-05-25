8130c4765bad346cdbf7204dee79435f9b680a77
Natalia Davydova <harmfuly7@gmail.com>
2025-05-25 16:30:21 +0300

    feat: updated index.js

--- src/routes/graphql/index.ts
+++ src/routes/graphql/index.ts
@@ -1,9 +1,40 @@
 import { FastifyPluginAsyncTypebox } from '@fastify/type-provider-typebox';
 import { createGqlResponseSchema, gqlResponseSchema } from './schemas.js';
 import { graphql } from 'graphql';
+import { schema } from './schema.js';
 
 const plugin: FastifyPluginAsyncTypebox = async (fastify) => {
   const { prisma } = fastify;
+
+  const rootValue = {
+    memberTypes: () => prisma.memberType.findMany(),
+    memberType: ({ id }: { id: string }) => prisma.memberType.findUnique({ where: { id } }),
+    users: () => prisma.user.findMany(),
+    user: ({ id }: { id: string }) => prisma.user.findUnique({ where: { id } }),
+    posts: () => prisma.post.findMany(),
+    post: ({ id }: { id: string }) => prisma.post.findUnique({ where: { id } }),
+    profiles: () => prisma.profile.findMany(),
+    profile: ({ id }: { id: string }) => prisma.profile.findUnique({ where: { id } }),
+
+    createUser: ({ dto }: { dto: any }) => prisma.user.create({ data: dto }),
+    createProfile: ({ dto }: { dto: any }) => prisma.profile.create({ data: dto }),
+    createPost: ({ dto }: { dto: any }) => prisma.post.create({ data: dto }),
+    changePost: ({ id, dto }: { id: string; dto: any }) => prisma.post.update({ where: { id }, data: dto }),
+    changeProfile: ({ id, dto }: { id: string; dto: any }) => prisma.profile.update({ where: { id }, data: dto }),
+    changeUser: ({ id, dto }: { id: string; dto: any }) => prisma.user.update({ where: { id }, data: dto }),
+    deleteUser: async ({ id }: { id: string }) => {
+      await prisma.user.delete({ where: { id } });
+      return 'User deleted';
+    },
+    deletePost: async ({ id }: { id: string }) => {
+      await prisma.post.delete({ where: { id } });
+      return 'Post deleted';
+    },
+    deleteProfile: async ({ id }: { id: string }) => {
+      await prisma.profile.delete({ where: { id } });
+      return 'Profile deleted';
+    },
+  };
 
   fastify.route({
     url: '/',
@@ -15,9 +46,23 @@ const plugin: FastifyPluginAsyncTypebox = async (fastify) => {
       },
     },
     async handler(req) {
-      // return graphql();
-    },
+      const { query, variables, operationName } = req.body as {
+        query: string;
+        variables?: Record<string, any>;
+        operationName?: string;
+      };
+
+      const result = await graphql({
+        schema,
+        source: query,
+        rootValue,
+        variableValues: variables,
+        operationName,
+        contextValue: { prisma, fastify }
   });
+  return result;
+},
+});
 };
 
 export default plugin;
